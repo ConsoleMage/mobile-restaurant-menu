61eb042771efa32b1b583e00278b5f9321b79933
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2025-01-10 20:14:40 +0800

    second half day

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
 import { menuArray } from "./data.js";
 
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     if (e.target.closest('.add-btn')) {
         const itemId = e.target.closest('.add-btn').dataset.id;
         if (itemId) {
             handleAddItem(Number(itemId));
         }
+    } else if (e.target.closest(".remove-btn")) {
+        console.log("remove btn clicked");
+        const targetDiv = e.target.closest(".remove-btn").dataset.remove;
+        if (targetDiv) {
+            targetDiv.remove();
+        }
     }
 });
 
-let toggle = false;
+let ordersHtml = ``;
+let orders = "";
 
 function handleAddItem(itemId) {
+    let hidden = "";
     const targetItemObj = menuArray.filter(menuItem => menuItem.id === itemId)[0];
-    console.log(targetItemObj);
-    
-    if (!toggle) {
-        document.getElementById("checkout").classList.toggle('hidden');
-    }
-    toggle = true;
-}
 
+    orders += `
+        <div class="orders-list">
+            <div class="item-title">${targetItemObj.name}</div>
+            <div class="remove-btn data-remove">remove</div> 
+            <div class="order-price">$${targetItemObj.price}</div>
+        </div>
+    `;
+
+    ordersHtml = `
+    <section class="${hidden} checkout">
+        <div class="checkout-title">Your order</div>
+            ${orders}
+        <hr class="orders-divider">
+    </section>
+    `;
+
+    render();
+}
 
 function getOrderHtml() {
     let menuHtml = ``;
-    let ordersHtml = ``;
-
-    ordersHtml += `
-    <section class="hidden" id="checkout">
-        <div class="checkout-title">Your order</div>
-    </section>
-    `;
-    console.log(ordersHtml);
 
     menuArray.forEach(function (menuItem) {
         menuHtml += `
         <section class="item">
             <div class="item-graphic">${menuItem.emoji}</div>
-            <div class="container">
+            <div class="item-container">
                 <div class="item-title">${menuItem.name}</div>
                 <div class="item-description">${menuItem.ingredients}</div>
                 <div class="item-price">$${menuItem.price}</div>
@@ -50,12 +61,11 @@ function getOrderHtml() {
         `;
     });
 
+    console.log(ordersHtml);
     menuHtml += ordersHtml;
-    
+
     return menuHtml;
 }
-
-console.log(getOrderHtml());
 
 function render() {
     document.getElementById("main").innerHTML = getOrderHtml();
